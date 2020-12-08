9568d1da9538dd34b0c6853cd9041e213ad0d5d3
Vincent Mackay <mackv42@gmail.com>
2020-12-08 13:41:45 -0600

    move drawing stuff to draw.js

---
+++ draw.js
@@ -0,0 +1,34 @@
+let canvas = document.getElementById("editor");
+let context = canvas.getContext("2d");
+
+function clear(){
+	context.fillStyle = "white";
+	context.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+function reloadImage(){
+	context.drawImage(spriteData.image, 0, 0);
+}
+
+function drawBox(box){
+	context.fillStyle = "rgba(189, 195, 199, 150)";
+	context.globalAlpha = 0.5;
+		let width = box.point2.x - box.point1.x;
+		let height = box.point2.y - box.point1.y;
+		context.fillRect(box.point1.x, box.point1.y, width, height);
+
+		if(box.selected){
+			context.beginPath();
+			context.lineWidth = "3";
+			context.strokeStyle = "green";
+			context.rect(box.point1.x, box.point1.y, width, height);
+			context.stroke();
+		} else{
+			context.beginPath();
+			context.lineWidth = "1";
+			context.strokeStyle = "red";
+			context.rect(box.point1.x, box.point1.y, width, height);
+			context.stroke();
+		}
+	context.globalAlpha = 1;
+}
--- sprites.js
+++ sprites.js
@@ -1,5 +1,3 @@
-let canvas = document.getElementById("editor");
-let context = canvas.getContext("2d");
 
 let spriteData = {
 	"src": "",
@@ -75,39 +73,6 @@ function imageIsLoaded() {
 	editorState.originalImage = context.getImageData(0, 0, canvas.width, canvas.height);
   }
 }
-
-function clear(){
-	context.fillStyle = "white";
-	context.fillRect(0, 0, canvas.width, canvas.height);
-}
-
-function reloadImage(){
-	context.drawImage(spriteData.image, 0, 0);
-}
-
-function drawBox(box){
-	context.fillStyle = "rgba(189, 195, 199, 150)";
-	context.globalAlpha = 0.5;
-		let width = box.point2.x - box.point1.x;
-		let height = box.point2.y - box.point1.y;
-		context.fillRect(box.point1.x, box.point1.y, width, height);
-
-		if(box.selected){
-			context.beginPath();
-			context.lineWidth = "3";
-			context.strokeStyle = "green";
-			context.rect(box.point1.x, box.point1.y, width, height);
-			context.stroke();
-		} else{
-			context.beginPath();
-			context.lineWidth = "1";
-			context.strokeStyle = "red";
-			context.rect(box.point1.x, box.point1.y, width, height);
-			context.stroke();
-		}
-	context.globalAlpha = 1;
-}
-
 
 function drawSelectBoxes(){
 	let boxes = editorState.frames;
